0681908f61b44ac2d06f7c28ce46628544877e55
Bruno M <34241947+stamby@users.noreply.github.com>
2020-07-14 17:00:59 -0300

    Update mention.py

--- commands/mention.py
+++ commands/mention.py
@@ -5,7 +5,7 @@ async def run(prefix_, message):
             random.choice((
                 '<@!%d>: What is your command? Don\'t they begin with _%s?_',
                 '<@!%d>: Yes? Do you need help? _%sHelp_ might be what you\'re looking for.',
-                '<@!%d>: I\'m listening. Type _%shelp_ to get started._',
+                '<@!%d>: I\'m listening. Type _%shelp_ to get started.',
                 'Do you need anything, <@!%d>? _%s_ is the current prefix.',
                 'How may I help you, <@!%d>? Maybe check _%shelp?_',
                 'Ready when you are, <@!%d>. Just type _%shelp._')) \
