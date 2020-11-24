fb1fd2cbe70c1f0fc737a736428f5aac7d968748
Bruno <stamby@github.com>
2020-11-24 00:27:10 -0300

    Won't listen to events: fix

--- bot.py
+++ bot.py
@@ -7,22 +7,18 @@ from os import getenv
 from guild_client import GuildClient
 from message_client import MessageClient
 
+intents = Intents()
+intents.guilds = True
+intents.members = True
+intents.messages = True
+intents.reactions = True
+
 def launch_guild_client():
-    intents = Intents()
-
-    intents.guilds = True
-
     g = GuildClient(intents)
 
     g.run(getenv('TOKEN'))
 
 def launch_message_client():
-    intents = Intents()
-
-    intents.messages = True
-    intents.members = True
-    intents.reactions = True
-
     m = MessageClient(intents)
 
     m.run(getenv('TOKEN'))
