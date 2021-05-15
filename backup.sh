bbb84d8f486d33188e629309db32bb6758d1ff0e
Bruno <stamby@github.com>
2021-05-14 23:59:39 -0300

    Backup

--- backup.sh
+++ backup.sh
@@ -2,24 +2,26 @@
 
 ### Easy Pete Bot: back up & purge
 
+DATE=`date +%F-%H`
+
 BACKUP_DIR=/srv/easy-pete-backup/backup
-BACKUP_FILE=$BACKUP_DIR/`date +%F-%H`-easy-pete.bak
+BACKUP_FILE=$BACKUP_DIR/${DATE}-easy-pete.bak
 
 BACKUP_LOG_DIR=/srv/easy-pete-backup/log
-BACKUP_LOG=$BACKUP_LOG_DIR/`date +%F-%H`-easy-pete.log
+BACKUP_LOG=$BACKUP_LOG_DIR/${DATE}-easy-pete.out.log
+BACKUP_ERR_LOG=$BACKUP_LOG_DIR/${DATE}-easy-pete.err.log
 
-exec 2>&1 > $BACKUP_LOG
+exec 2> $BACKUP_ERR_LOG > $BACKUP_LOG
 
 # $1 = Number of files, $2 = Oldest file without directory name 
 purge() {
-    if [ $1 -gt 5 ]; then
+    while [ $1 -gt 5 ]; do
         # Remove backup
-        rm -v $BACKUP_DIR/$2
+        rm -v "$BACKUP_DIR/$2"
 
         # Also remove its log file
-        rm -v $BACKUP_LOG_DIR/`printf '%s' $2 | sed 's/\.bak$/.log/'`
-    fi
-
+        rm -v "$BACKUP_LOG_DIR/`printf '%s' $2 | sed 's/\.bak$//'`"*
+    done
 }
 
 back_up() {
