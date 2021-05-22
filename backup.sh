a589600594b13a2d5e58b2858227c49a58b02320
Bruno <stamby@github.com>
2021-05-22 15:48:23 -0300

    Backup correction

--- backup.sh
+++ backup.sh
@@ -8,20 +8,19 @@ BACKUP_DIR=/srv/easy-pete-backup/backup
 BACKUP_FILE=$BACKUP_DIR/${DATE}-easy-pete.bak
 
 BACKUP_LOG_DIR=/srv/easy-pete-backup/log
-BACKUP_LOG=$BACKUP_LOG_DIR/${DATE}-easy-pete.out.log
-BACKUP_ERR_LOG=$BACKUP_LOG_DIR/${DATE}-easy-pete.err.log
+BACKUP_LOG=$BACKUP_LOG_DIR/${DATE}-easy-pete.log
 
-exec 2> $BACKUP_ERR_LOG > $BACKUP_LOG
+exec 2>&1 > $BACKUP_LOG
 
 # $1 = Number of files, $2 = Oldest file without directory name 
 purge() {
-    while [ $1 -gt 5 ]; do
+    if [ $1 -gt 5 ]; then
         # Remove backup
         rm -v "$BACKUP_DIR/$2"
 
         # Also remove its log file
-        rm -v "$BACKUP_LOG_DIR/`printf '%s' $2 | sed 's/\.bak$//'`"*
-    done
+        rm -v "$BACKUP_LOG_DIR/`printf '%s' $2 | sed 's/\.bak$//'`"
+    fi
 }
 
 back_up() {
